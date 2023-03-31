12a6b8da71c6bbaa631e458126596f4b3c162c28
Hein Htet Win <heinhtetwin@ayainnovation.com>
2023-03-31 09:47:22 +0630

    run pipeline again

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is a sample static website Deployed by <i>Heinux</i>.
+          This is some more text added!.
         </p>
         <a
           className="App-link"
